5b96d3766fae48583c9d70ee4256f60b5adb8ba8
Ishtiaq Robin <robin.pass36@gmail.com>
2026-06-23 15:35:46 +0600

    range used in array, slice, string, map, channel

--- range/main.go
+++ range/main.go
@@ -1,7 +1,49 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	// map is a dynamic array
+	// myMap := map[string]string{
+	// 	"name":    "Ishtiaq",
+	// 	"success": "ok",
+	// }
+
+	// for _, value := range myMap {
+	// 	fmt.Println(value)
+	// }
+
+	// array is a static array
+	// myArr := [3]string{
+	// 	"red",
+	// 	"green",
+	// 	"blue",
+	// }
+
+	// for i, value := range myArr {
+	// 	fmt.Println(i, value)
+	// }
+
+	// slice is a dynamic array (memory allocation is done at runtime)
+	// colors := []string{
+	// 	"red",
+	// 	"green",
+	// 	"blue",
+	// }
+
+	// for i, color := range colors {
+	// 	fmt.Println(i, color)
+	// }
+
+	// string is a dynamic array (underline the bytes of the string)
+	name := "Next Level"
+
+	var byteSlice = []byte(name)
+	fmt.Println(byteSlice)
+
+	// for i, value := range name {
+	// 	fmt.Println(i, value)
+	// }
 }
 
 // array
