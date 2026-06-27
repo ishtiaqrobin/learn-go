5408a1cb1873731876e348dab050eaf29f5a1874
Ishtiaq Robin <robin.pass36@gmail.com>
2026-06-27 18:01:24 +0600

    learn enum with switch case and type

--- enum/main.go
+++ enum/main.go
@@ -2,6 +2,39 @@ package main
 
 import "fmt"
 
+type Weekday int
+
+const (
+	Sunday Weekday = iota // 0
+	Monday
+	Tuesday
+	Wednesday
+	Thursday
+	Friday
+	Saturday
+)
+
+func getWorkDayStatus(day Weekday) string {
+	switch day {
+	case Sunday, Monday, Tuesday, Wednesday:
+		return "Working day"
+	case Thursday:
+		return "Half day"
+	case Friday, Saturday:
+		return "Off day"
+	default:
+		return "Invalid day"
+	}
+}
+
+type OfficeStatus string
+
+const (
+	WorkingDay OfficeStatus = "open"
+	HalfDay    OfficeStatus = "half_day"
+	OffDay     OfficeStatus = "closed"
+)
+
 func main() {
-	fmt.Println("")
+	fmt.Println(getWorkDayStatus(Thursday))
 }
