f852ed6e7f8f68576de9e7538bbef5a6c3a31210
Ishtiaq Robin <robin.pass36@gmail.com>
2026-06-28 15:15:59 +0600

    os-vs-goroutine-thread -simulation file in depth

---
+++ channel/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var wg sync.WaitGroup
+
+func main() {
+	var ch = make(chan string) // unbuffered channel
+
+	go uploadFile(ch)
+
+	fileUrl := <-ch // blocking...
+
+	fmt.Println("File url :", fileUrl)
+
+}
+
+func uploadFile(c chan string) {
+	fmt.Println("uploading file...")
+	time.Sleep(time.Second * 3)
+	fmt.Println("file upload done!")
+
+	fileUrl := "https://www.google.com"
+	c <- fileUrl
+}
--- race-condition/main.go
+++ race-condition/main.go
@@ -21,7 +21,7 @@ func main() {
 }
 
 func increment() {
-	mu.Lock()
-	defer mu.Unlock()
-	counter++
+	mu.Lock()         // ১. দরজা লক করে ভেতরে ঢোকা
+	defer mu.Unlock() // ৩. ফাংশন শেষ হওয়ার মুহূর্তে দরজা খুলে দেওয়া
+	counter++         // ২. ক্রিপ্টোগ্রাফিক বা ক্রিটিক্যাল জোনের কাজ (Read-Modify-Write)
 }
